e563b0727b3ab008f39b7c4d98882a61c545887b
olavo argemiro <olaviochampions@gmail.com>
2021-07-06 19:32:43 -0300

    finalizando projeto v.1.0

--- projetos/kingston/script.js
+++ projetos/kingston/script.js
@@ -1,4 +1,5 @@
 let menumob = document.querySelector(".menu-mobile");
+let blocoCard = document.getElementById("bloco");
 function clicouMenu(){
 	if(menumob.classList.contains("d-none")){
 		menumob.classList.add("d-block");
@@ -8,3 +9,16 @@ function clicouMenu(){
 		menumob.classList.remove("d-block");
 	}
 } 
+	var distancia = 0;
+function apertouEsquerda(){
+	if(distancia > -560){
+	distancia = distancia -560;
+	blocoCard.style.marginRight = distancia+"px";
+	}
+}
+function apertouDireita(){
+	if(distancia < 560){
+	distancia = distancia + 560;
+	blocoCard.style.marginRight = distancia+"px";
+	}
+}
